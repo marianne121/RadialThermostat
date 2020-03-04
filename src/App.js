70bd7d75d3d4283f1debe0ab45285de25e9fe879
unknown <marianne.quek@gmail.com>
2020-03-04 19:19:05 +0800

    add scale and temp colour strip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Scale from './scale.png' 
 
 function App () {
   return (
     <div className="App">
-      <header className="App-header">
       <div className="radial-thermostat">
       <svg width="400" height="400">
       <RadialThermostat/>
       <CircularBackground/>
+      
       <ThermostatKnob/>
       </svg>
+        <div className="Dials">
+        <TemperatureDials/>
+        </div>
       </div>
-      </header>
     </div>
   )
 }
 class RadialThermostat extends React.Component{
   render() {
     return (
-        <circle cx="205" cy="205" r="175" fill="#f0f0f0"/>
+        <circle cx="225" cy="225" r="175" fill="#f0f0f0"/>
     );
   }
 }
@@ -28,7 +31,7 @@ class RadialThermostat extends React.Component{
 class CircularBackground extends React.Component{
   render() {
     return(
-        <circle cx="205" cy="205" r="165" fill="#ffffff"/>
+        <circle cx="225" cy="225" r="165" fill="#ffffff"/>
     );
   }
 }
@@ -36,8 +39,34 @@ class CircularBackground extends React.Component{
 class ThermostatKnob extends React.Component{
   render() {
     return (
-      <circle cx="205" cy="205" r="155" fill="#50505a"/>
+      
+      <React.Fragment>
+            <linearGradient id="grayColour">
+       <stop offset="0%" stop-color="#353b3f"></stop>
+       <stop offset="100%" stop-color="#353b3f"></stop>
+    </linearGradient>
+    <linearGradient id="gradientColour" >
+       <stop offset="0%" stop-color="#4a92d8"></stop>
+       <stop offset="100%" stop-color="#e06b71"></stop>
+    </linearGradient>
+    <path d="
+          M140 355 
+          A155 155 0 1 1 310 355"
+        fill="none" stroke="url(#gradientColour)" stroke-width="6" />
+   <path d="M140 355 
+        A155 155 0 0 0 310 355"
+        fill="none" stroke="url(#grayColour)" stroke-width="6" />
+        <circle cx="224.5" cy="225.5" r="152.5" fill="#50505a"/>
+      </React.Fragment>
     );
+  }
+}
+
+class TemperatureDials extends React.Component{
+  render() {
+    return (
+      <img src={Scale} alt="tempScale" width="287" height="287"/>
+    )
   }
 }
 
