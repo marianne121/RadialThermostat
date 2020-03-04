5b32cdca29646e3e2ecaef82f90a9f9ca80843f7
unknown <marianne.quek@gmail.com>
2020-03-04 15:46:49 +0800

    add thermostat background

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="radial-thermostat">
+      <svg width="400" height="400">
+      <RadialThermostat/>
+      <CircularBackground/>
+      <ThermostatKnob/>
+      </svg>
+      </div>
       </header>
     </div>
-  );
+  )
+}
+class RadialThermostat extends React.Component{
+  render() {
+    return (
+        <circle cx="205" cy="205" r="175" fill="#f0f0f0"/>
+    );
+  }
+}
+
+class CircularBackground extends React.Component{
+  render() {
+    return(
+        <circle cx="205" cy="205" r="165" fill="#ffffff"/>
+    );
+  }
+}
+
+class ThermostatKnob extends React.Component{
+  render() {
+    return (
+      <circle cx="205" cy="205" r="155" fill="#50505a"/>
+    );
+  }
 }
 
 export default App;
